0a829e136c86be4c03ae7070cb385ecb885ceb86
Connor Jensen <connor.jensen@live.com>
2021-06-14 08:48:54 -0600

    add all players to waitConfirmation stage

--- src/gameLogic/phases/waitingRoom.js
+++ src/gameLogic/phases/waitingRoom.js
@@ -1,20 +1,29 @@
 export const waitingRoom = {
    start: true,
+   onStart: (G, ctx) => {
+      ctx.events.setActivePlayers({all: 'waitConfirmation', moveLimit: 1})
+   },
    endIf: (G) => {
       let numPlayersReady = G.playersReady.filter((isReady) => isReady).length;
       console.log(`numPlayersReady: ${numPlayersReady}`);
-      return (numPlayersReady === 5)
+      return numPlayersReady === 5;
    },
    onEnd: (G, ctx) => {
       for (let i = 0; i < 5; i++) {
          G.playersReady[i] = false;
       }
    },
-   moves: {
-      confirmReady: (G, ctx) => {
-         G.playersReady[ctx.currentPlayer] = true;
-         ctx.events.endTurn();
+   turn: {
+      stages: {
+         waitConfirmation: {
+            moves: {
+               confirmReady: (G, ctx) => {
+                  G.playersReady[ctx.currentPlayer] = true;
+                  ctx.events.endTurn();
+               },
+            },
+         },
       },
    },
-   next: 'bidding'
-}
+   next: "bidding",
+};
